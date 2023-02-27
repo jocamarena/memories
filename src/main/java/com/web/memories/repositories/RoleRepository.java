2c683f7dfadc106f461fa192b422498ea7c8e9d9
Jose Camarena <jose.j.camarena@gmail.com>
2023-02-26 17:46:51 -0800

    add user and role repositories

---
+++ src/main/java/com/web/memories/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.web.memories.repositories;
+
+import com.web.memories.domain.users.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
